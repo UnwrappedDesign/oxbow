e7e8f4c6e518be7b1d30a486b0269ea08302759a
Gabriel Perales <gabriel.perales@cabify.com>
2024-06-08 01:30:47 +0200

    added rename plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import rename from 'astro-rename';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +18,5 @@ export default defineConfig({
     drafts: true
   },
   site: 'https://windstatic.com',
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [tailwind(), rename(), sitemap(), mdx()]
 });
