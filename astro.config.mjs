73935ce55b6b84fa0e289c87394aee3f9a553667
Gabriel Perales <gabriel.perales@cabify.com>
2024-06-08 01:30:00 +0200

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
 import alpinejs from "@astrojs/alpinejs";
+import rename from 'astro-rename';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,9 +20,9 @@ export default defineConfig({
     drafts: true
   },
   site: 'https://windstatic.com',
-  integrations: [tailwind(), sitemap(), mdx(), alpinejs()],
+  integrations: [tailwind(), rename(), sitemap(), mdx(), alpinejs()],
   adapter: node({
     mode: 'standalone'
   }),
-  output: 'server'
+  output: 'static'
 });
