7880edbd47578eebf0896930f3c991e7680c512e
Gabriel Perales <gabriel.perales@cabify.com>
2024-06-08 02:55:22 +0200

    added middleware to protect routes

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { defineMiddleware } from "astro/middleware";
+
+import { getAuth } from "firebase-admin/auth";
+import { app } from "./firebase/server";
+
+const protectedRoutes = [
+  "/blog-content/",
+  "/blog-entries/",
+  "/centered-headings/",
+  "/centered-heros/",
+  "/faq/",
+  "/features/",
+  "/grids/",
+  "/left-headings/",
+  "/left-heros/",
+  "/pricing/",
+  "/right-headings/",
+  "/right-heros/",
+  "/team/",
+];
+
+export const onRequest = defineMiddleware(async (context, next) => {
+  const pathname = context.url.pathname;
+  
+  if (protectedRoutes.every(route => !pathname.startsWith(route))){
+    return next();
+  }
+  
+  try {
+    if (context.cookies.has("__session")) {
+      const auth = getAuth(app);
+      const sessionCookie = context.cookies.get("__session").value;
+      const decodedCookie = await auth.verifySessionCookie(sessionCookie);
+      if (decodedCookie) {
+        return next();
+      }
+    }
+    // forward request
+  } catch (error) {}
+  return Response.redirect(new URL("/pricing", context.url), 302);
+});
