f676ae6afe1dd7d8722c98a2df3b45e11cb7896b
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-06-14 23:47:02 +0300

    added lots of thumbnails

--- src/data/entries.json.ts
+++ src/data/entries.json.ts
@@ -4,6 +4,12 @@ export interface Template {
   quantity: number;
   image: string;
 }
+// MArketing
+//
+//
+//
+// Page sections
+//
 const leftheroes: Template = {
   name: "Left Heroes",
   quantity: 20,
@@ -84,10 +90,284 @@ const blogContent: Template = {
 };
 const testimonials: Template = {
   name: "Testimonials",
-  quantity: 5,
+  quantity: 20,
   link: "/playground/marketing/blog-content",
   image: "testimonials.png",
 };
-export const general: Template[] = [leftheroes, centeredheroes, rightheroes, features, leftHeadings, centeredHeadings, rightHeadings, grids, pricing, faq, team, blogEntries, blogContent, testimonials];
-export const byName = { leftheroes, centeredheroes, rightheroes, features, leftHeadings, centeredHeadings, rightHeadings, grids, pricing, faq, team, blogEntries, blogContent, testimonials };
+const gallery: Template = {
+  name: "Galleries",
+  quantity: 1,
+  link: "/playground/marketing/gallery",
+  image: "gallery.png",
+};
+const logoclouds: Template = {
+  name: "Logo Clouds",
+  quantity: 1,
+  link: "/playground/marketing/logo-clouds",
+  image: "logoclouds.png",
+};
+const stats: Template = {
+  name: "Stats",
+  quantity: 1,
+  link: "/playground/marketing/stats",
+  image: "stats.png",
+};
+const footers: Template = {
+  name: "Footers",
+  quantity: 1,
+  link: "/playground/marketing/footers",
+  image: "footers.png",
+};
+const status: Template = {
+  name: "Status",
+  quantity: 1,
+  link: "/playground/marketing/status",
+  image: "status.png",
+};
+const contact: Template = {
+  name: "Contact",
+  quantity: 1,
+  link: "/playground/marketing/contact",
+  image: "contact.png",
+};
+const newsletter: Template = {
+  name: "Newsletter",
+  quantity: 1,
+  link: "/playground/marketing/newsletter",
+  image: "newsletter.png",
+};
+
+// Application
+//
+//
+//
+
+// Navigation
+const navbars: Template = {
+  name: "Navbars",
+  quantity: 1,
+  link: "/playground/application/navbars",
+  image: "navbars.png",
+};
+const flyouts: Template = {
+  name: "Flyout menus",
+  quantity: 1,
+  link: "/playground/application/flyouts",
+  image: "flyouts.png",
+};
+const megamenu: Template = {
+  name: "Mega Menu",
+  quantity: 1,
+  link: "/playground/application/megamenu",
+  image: "megamenu.png",
+};
+const sidebars: Template = {
+  name: "Sidebars",
+  quantity: 1,
+  link: "/playground/application/sidebars",
+  image: "sidebars.png",
+};
+const verticalnav: Template = {
+  name: "Vertical navigation",
+  quantity: 1,
+  link: "/playground/application/verticalnav",
+  image: "verticalnav.png",
+};
+const breadcrumbs: Template = {
+  name: "Breadcrumbs",
+  quantity: 1,
+  link: "/playground/application/breadcrumbs",
+  image: "breadcrumbs.png",
+};
+const pagination: Template = {
+  name: "Pagination",
+  quantity: 1,
+  link: "/playground/application/pagination",
+  image: "pagination.png",
+};
+const tabs: Template = {
+  name: "Tabs",
+  quantity: 1,
+  link: "/playground/application/tabs",
+  image: "tabs.png",
+};
+const commandbar: Template = {
+  name: "Commandbar",
+  quantity: 1,
+  link: "/playground/application/commandbar",
+  image: "commandbar.png",
+};
+// Elements
+//
+const banners: Template = {
+  name: "Banners",
+  quantity: 5,
+  link: "/playground/marketing/banners",
+  image: "banners.png",
+};
+const buttons: Template = {
+  name: "Buttons",
+  quantity: 1,
+  link: "/playground/application/buttons",
+  image: "buttons.png",
+};
+const buttongroups: Template = {
+  name: "Button Groups",
+  quantity: 1,
+  link: "/playground/application/buttongroups",
+  image: "buttongroups.png",
+};
+const badges: Template = {
+  name: "Badges",
+  quantity: 1,
+  link: "/playground/application/badges",
+  image: "badges.png",
+};
+const avatars: Template = {
+  name: "Avatars",
+  quantity: 1,
+  link: "/playground/application/avatars",
+  image: "avatars.png",
+};
+// Overlays
+const modals: Template = {
+  name: "Modals",
+  quantity: 1,
+  link: "/playground/application/modals",
+  image: "modals.png",
+};
+const slideover: Template = {
+  name: "Slideover",
+  quantity: 1,
+  link: "/playground/application/slideover",
+  image: "slideover.png",
+};
+const notifications: Template = {
+  name: "Notifications",
+  quantity: 1,
+  link: "/playground/application/notifications",
+  image: "notifications.png",
+};
+// Forms
+const formlayouts: Template = {
+  name: "Form Layouts",
+  quantity: 1,
+  link: "/playground/application/formlayouts",
+  image: "formlayouts.png",
+};
+const inputs: Template = {
+  name: "Inputs",
+  quantity: 1,
+  link: "/playground/application/inputs",
+  image: "inputs.png",
+};
+
+
+// Ecommerce
+//
+//
+//
+
+// Marketing
+//
+//
+//
+export const pageSections: Template[] = [
+  leftheroes,
+  centeredheroes,
+  rightheroes,
+  features,
+  leftHeadings,
+  centeredHeadings,
+  rightHeadings,
+  grids,
+  pricing,
+  faq,
+  team,
+  blogEntries,
+  blogContent,
+  testimonials,
+  gallery,
+  logoclouds,
+  stats,
+  footers,
+  status,
+  contact,
+  newsletter,
+];
+export const elements: Template[] = [banners, buttons, buttongroups, badges, avatars];
+
+// Application
+//
+//
+//
+export const navigation: Template[] = [
+  navbars,
+  flyouts,
+  megamenu,
+  sidebars,
+  verticalnav,
+  breadcrumbs,
+  pagination,
+  tabs,
+  commandbar,
+];
+export const overlay: Template[] = [notifications, modals, slideover, ];
+export const forms: Template[] = [formlayouts, inputs, ];
+
+// Ecommerce
+//
+//
+//
+// All by name
+//
+//
+//
+export const byName = {
+  // Page sections
+  leftheroes,
+  centeredheroes,
+  rightheroes,
+  features,
+  leftHeadings,
+  centeredHeadings,
+  rightHeadings,
+  grids,
+  pricing,
+  faq,
+  team,
+  blogEntries,
+  blogContent,
+  testimonials,
+  gallery,
+  logoclouds,
+  stats,
+  footers,
+  status,
+  contact,
+  newsletter,
+  // Elements
+  banners,
+  buttons,
+  buttongroups,
+  badges,
+  avatars,
+  // Navigation
+  navbars,
+  flyouts,
+  megamenu,
+  sidebars,
+  verticalnav,
+  breadcrumbs,
+  pagination,
+  tabs,
+  commandbar,
+  // Overlays
+  notifications,
+  slideover,
+  modals,
+  // Forms
+  formlayouts,
+  inputs,
+};
 export const allEntries = Object.values(byName);
