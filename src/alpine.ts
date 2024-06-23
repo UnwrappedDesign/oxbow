e696e2327741a40b43e14dce293f5141fcc836be
Gabriel Perales <gabriel.perales@cabify.com>
2024-06-23 23:41:12 +0200

    added lazy loading to iframes

--- src/alpine.ts
+++ src/alpine.ts
@@ -2,9 +2,11 @@ import type { Alpine } from 'alpinejs'
 import ui from '@alpinejs/ui'
 import focus from '@alpinejs/focus'
 import Clipboard from "@ryangjchandler/alpine-clipboard"
+import intersect from '@alpinejs/intersect'
 
 export default (Alpine: Alpine) => {
     Alpine.plugin(ui)
     Alpine.plugin(focus)
     Alpine.plugin(Clipboard)
+    Alpine.plugin(intersect)
 }
