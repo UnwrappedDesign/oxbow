20569ec89116933cc945b7d31c2ff6ea9a401b38
Gabriel Perales <gabriel.perales@cabify.com>
2024-06-24 20:30:02 +0200

    clean up

--- src/data/entries.json.ts
+++ src/data/entries.json.ts
@@ -1,7 +1,6 @@
 export interface Template {
   link: string;
   name: string;
-  quantity: number;
   image: string;
 }
 // MArketing
@@ -12,127 +11,106 @@ export interface Template {
 //
 const leftheroes: Template = {
   name: "Left Heroes",
-  quantity: 20,
   link: "/playground/marketing/left-heroes",
-  image: "leftheroes.png",
+  image: "leftHeroes.png",
 };
 const centeredheroes: Template = {
   name: "Centered Heroes",
-  quantity: 20,
   link: "/playground/marketing/centered-heroes",
-  image: "centeredheroes.png",
+  image: "centeredHeroes.png",
 };
 const rightheroes: Template = {
   name: "Right Heroes",
-  quantity: 20,
   link: "/playground/marketing/right-heroes",
-  image: "rightheroes.png",
+  image: "rightHeroes.png",
 };
 const features: Template = {
   name: "Features",
-  quantity: 30,
   link: "/playground/marketing/features",
   image: "features.png",
 };
 const leftHeadings: Template = {
   name: "Left Headings",
-  quantity: 20,
   link: "/playground/marketing/left-headings",
   image: "leftHeadings.png",
 };
 const centeredHeadings: Template = {
   name: "Centered Headings",
-  quantity: 20,
   link: "/playground/marketing/centered-headings",
   image: "centeredHeadings.png",
 };
 const rightHeadings: Template = {
   name: "Right Headings",
-  quantity: 20,
   link: "/playground/marketing/right-headings",
   image: "rightHeadings.png",
 };
 const grids: Template = {
   name: "Grids",
-  quantity: 20,
   link: "/playground/marketing/grids",
   image: "grids.png",
 };
 const pricing: Template = {
   name: "Pricing",
-  quantity: 20,
   link: "/playground/marketing/pricing",
   image: "pricing.png",
 };
 const faq: Template = {
   name: "Faqs",
-  quantity: 20,
   link: "/playground/marketing/faq",
   image: "faq.png",
 };
 const team: Template = {
   name: "Team",
-  quantity: 20,
   link: "/playground/marketing/team",
   image: "team.png",
 };
 const blogEntries: Template = {
   name: "Blog Entries",
-  quantity: 10,
   link: "/playground/marketing/blog-entries",
   image: "blogEntries.png",
 };
 const blogContent: Template = {
   name: "Blog Content",
-  quantity: 5,
   link: "/playground/marketing/blog-content",
   image: "blogContent.png",
 };
 const testimonials: Template = {
   name: "Testimonials",
-  quantity: 20,
   link: "/playground/marketing/testimonials",
   image: "testimonials.png",
 };
 const gallery: Template = {
   name: "Galleries",
-  quantity: 10,
   link: "/playground/marketing/gallery",
   image: "gallery.png",
 };
 const logoclouds: Template = {
   name: "Logo Clouds",
-  quantity: 1,
   link: "/playground/marketing/logo-clouds",
   image: "logoclouds.png",
 };
 const stats: Template = {
   name: "Stats",
-  quantity: 1,
   link: "/playground/marketing/stats",
   image: "stats.png",
 };
 const footers: Template = {
   name: "Footers",
-  quantity: 1,
   link: "/playground/marketing/footers",
   image: "footers.png",
 };
 const status: Template = {
   name: "Status",
-  quantity: 1,
   link: "/playground/marketing/status",
   image: "status.png",
 };
 const contact: Template = {
   name: "Contact",
-  quantity: 1,
   link: "/playground/marketing/contact",
   image: "contact.png",
 };
 const newsletter: Template = {
   name: "Newsletter",
-  quantity: 1,
   link: "/playground/marketing/newsletter",
   image: "newsletter.png",
 };
@@ -145,55 +123,46 @@ const newsletter: Template = {
 // Navigation
 const navbars: Template = {
   name: "Navbars",
-  quantity: 1,
   link: "/playground/application/navbars",
   image: "navbars.png",
 };
 const flyouts: Template = {
   name: "Flyout menus",
-  quantity: 1,
   link: "/playground/application/flyouts",
   image: "flyouts.png",
 };
 const megamenu: Template = {
   name: "Mega Menu",
-  quantity: 1,
   link: "/playground/application/megamenu",
   image: "megamenu.png",
 };
 const sidebars: Template = {
   name: "Sidebars",
-  quantity: 1,
   link: "/playground/application/sidebars",
   image: "sidebars.png",
 };
 const verticalnav: Template = {
   name: "Vertical navigation",
-  quantity: 1,
   link: "/playground/application/verticalnav",
   image: "verticalnav.png",
 };
 const breadcrumbs: Template = {
   name: "Breadcrumbs",
-  quantity: 1,
   link: "/playground/application/breadcrumbs",
   image: "breadcrumbs.png",
 };
 const pagination: Template = {
   name: "Pagination",
-  quantity: 1,
   link: "/playground/application/pagination",
   image: "pagination.png",
 };
 const tabs: Template = {
   name: "Tabs",
-  quantity: 1,
   link: "/playground/application/tabs",
   image: "tabs.png",
 };
 const commandbar: Template = {
   name: "Command Bar",
-  quantity: 1,
   link: "/playground/application/commandbar",
   image: "commandbar.png",
 };
@@ -201,105 +170,88 @@ const commandbar: Template = {
 //
 const banners: Template = {
   name: "Banners",
-  quantity: 5,
   link: "/playground/marketing/banners",
   image: "banners.png",
 };
 const buttons: Template = {
   name: "Buttons",
-  quantity: 1,
   link: "/playground/application/buttons",
   image: "buttons.png",
 };
 const buttongroups: Template = {
   name: "Button Groups",
-  quantity: 1,
   link: "/playground/application/buttongroups",
   image: "buttongroups.png",
 };
 const badges: Template = {
   name: "Badges",
-  quantity: 1,
   link: "/playground/application/badges",
   image: "badges.png",
 };
 const avatars: Template = {
   name: "Avatars",
-  quantity: 1,
   link: "/playground/application/avatars",
   image: "avatars.png",
 };
 const alerts: Template = {
   name: "Alerts",
-  quantity: 1,
   link: "/playground/application/alerts",
   image: "alerts.png",
 };
 const emptystates: Template = {
   name: "Emptystates",
-  quantity: 1,
   link: "/playground/application/emptystates",
   image: "emptystates.png",
 };
 // Overlays
 const modals: Template = {
   name: "Modals",
-  quantity: 1,
   link: "/playground/application/modals",
   image: "modals.png",
 };
 const slideover: Template = {
   name: "Slideover",
-  quantity: 1,
   link: "/playground/application/slideover",
   image: "slideover.png",
 };
 const notifications: Template = {
   name: "Notifications",
-  quantity: 1,
   link: "/playground/application/notifications",
   image: "notifications.png",
 };
 // Forms
 const formlayouts: Template = {
   name: "Form Layouts",
-  quantity: 1,
   link: "/playground/application/formlayouts",
   image: "formlayouts.png",
 };
 const inputs: Template = {
   name: "Inputs",
-  quantity: 1,
   link: "/playground/application/inputs",
   image: "inputs.png",
 };
 const textarea: Template = {
   name: "Textarea",
-  quantity: 1,
   link: "/playground/application/textarea",
   image: "textarea.png",
 };
 const checkboxes: Template = {
   name: "Checkboxes",
-  quantity: 1,
   link: "/playground/application/checkboxes",
   image: "checkboxes.png",
 };
 const toggles: Template = {
   name: "Toggles",
-  quantity: 1,
   link: "/playground/application/toggles",
   image: "toggles.png",
 };
 const selects: Template = {
   name: "selects",
-  quantity: 1,
   link: "/playground/application/selects",
   image: "selects.png",
 };
 const radiogroups: Template = {
   name: "Radio Groups",
-  quantity: 1,
   link: "/playground/application/radiogroups",
   image: "radiogroups.png",
 };
