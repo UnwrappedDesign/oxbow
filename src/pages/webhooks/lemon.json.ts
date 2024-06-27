91655b4e64d500ed6946be58b153621b3dc47cca
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-06-27 06:53:15 +0300

    added: animation on email-signin - fixed: 11 -12 -13 left heros

--- src/pages/webhooks/lemon.json.ts
+++ src/pages/webhooks/lemon.json.ts
@@ -2,43 +2,31 @@ import type { APIRoute } from 'astro';
 import { getAuth } from "firebase-admin/auth";
 import { createHmac } from "crypto";
 import { app } from "../../firebase/server";
-
 const secret = import.meta.env.LEMON_SQUEEZE_SECRET;
-
 function checkSignature(signature: string, payload: string) {
   return createHmac('sha256', secret).update(payload).digest('hex') === signature;
 }
-
-
 export const POST: APIRoute = async ({ request }) => {
   const auth = getAuth(app)
   const { headers } = request;
-
   const signature = headers.get('X-Signature');
   const body = await request.text();
-
   if (!signature) {
     return new Response('X-Signature header is required', { status: 400 });
   }
-
   if (signature && !checkSignature(signature, body)) {
     return new Response('Invalid X-Signature header', { status: 403 });
   }
-
   try {
     const json = JSON.parse(body);
-
     const { user_email: email } = json.data.attributes;
-
     if (!email) {
       return new Response('Email is required', { status: 400 });
     }
-
     const user = await auth.createUser({
       email,
       displayName: email,
     });
-
     return new Response(`User created: ${user.uid}`, { status: 200 });
   } catch (error) {
     console.error('Error creating user:', error);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,13 +12,11 @@ module.exports = {
       blackOnWhite: "0 10px 15px -3px rgba(0, 0, 0, 0.3), 0 4px 6px -4px rgba(0, 0, 0, 0.3) !important",
       whiteOnWhite: "0 8px 12px -3px rgba(0, 0, 0, 0.05), 0 3px 5px -4px rgba(0, 0, 0, 0.05) !important"
      },
-
     extend: {
       colors: {
         white: colors.white,
         gray: colors.gray,
         black: "#121212",
-
          blue: {
             '50': 'hsl(225, 100%, 97%)',
             '100': 'hsl(225, 95%, 93%)',
@@ -32,10 +30,8 @@ module.exports = {
             '900': 'hsl(234, 64%, 33%)',
             '950': 'hsl(236, 57%, 21%)',
             '1000': 'hsl( 239, 51%, 18%)',
-
           },
       },
-
     },
   },
   plugins: [
