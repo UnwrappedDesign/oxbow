1a9e40b45ce77e57d5725534ee7b5176524e7a4c
Gabriel Perales <gabriel.perales@cabify.com>
2024-07-05 22:06:42 +0200

    updated tests

---
+++ src/utils/verifySignture.spec.ts
@@ -0,0 +1,19 @@
+import { createHmac } from "crypto";
+import { verifySignature } from "./verifySignature";
+
+describe('verifySignature', () => {
+  it('should return true if the signature is valid', () => {
+    const payload = 'hello world';
+    const secret = 'secret';
+    const signature = createHmac('sha256', secret).update(payload).digest('hex');
+    expect(verifySignature(signature, payload, secret)).to.be.true;
+  });
+
+
+  it('should return false if the signature is invalid', () => {
+    const payload = 'hello world';
+    const secret = 'secret';
+    const signature = 'invalid';
+    expect(verifySignature(signature, payload, secret)).to.be.false;
+  });
+});
