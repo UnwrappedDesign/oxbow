5de44295c8b931b50bd50be04310d9a6d3275cee
Gabriel Perales <gabriel.perales@cabify.com>
2024-07-09 00:46:56 +0200

    hotfix

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -4,27 +4,35 @@ import {
   connectAuthEmulator,
 } from "firebase/auth";
 
+const apiKey = import.meta.env.PUBLIC_FIREBASE_API_KEY;
+const authDomain = import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN;
+const projectId = import.meta.env.PUBLIC_FIREBASE_PROJECT_ID;
+const storageBucket = import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+const appId = import.meta.env.PUBLIC_FIREBASE_APP_ID;
+
 const firebaseConfig: FirebaseOptions = {
-  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 };
 
 export const app = initializeApp(firebaseConfig);
+
 const authEmulatorHost = import.meta.env.PUBLIC_FIREBASE_AUTH_EMULATOR_HOST;
 
 export const auth = getAuth(app);
 
 if (authEmulatorHost) {
   try {
+    console.log('Connecting to auth emulatorHost.');
     connectAuthEmulator(auth, `http://${authEmulatorHost}`);
 
   } catch (error) {
     console.error(error);
   }
-  
 }
 
