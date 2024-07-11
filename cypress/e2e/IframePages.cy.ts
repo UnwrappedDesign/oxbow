bd3446f122d6920d8b33c08dfc088fc01451a446
Gabriel Perales <gabriel.perales@cabify.com>
2024-07-11 23:54:38 +0200

    added webhook test

--- cypress/e2e/IframePages.cy.ts
+++ cypress/e2e/IframePages.cy.ts
@@ -1,18 +1,21 @@
+import { TestComponentIframePage } from "cypress/pages/TestComponentIframePage";
+
 describe('IframePages', () => {
+  const testComponentIframePage = new TestComponentIframePage();
   const classArr = 'text-4xl mt-4 text-gray-900 lg:text-5xl lg:text-balance'.split(' ');
 
   describe('Unlogged user test component', () => {
     beforeEach(() => {
-      cy.visit('/iframe/src/components/windstatic/__test__/test.astro');
+      testComponentIframePage.visit()
     });
 
     it('should contain an h1 including the text "Hello world"', () => {
-      const $h1 = cy.findByRole('heading', { level: 1 });
+      const $h1 = testComponentIframePage.getHeading();
       $h1.should('include.text', 'Hello world');
     });
 
     it('shouldn\'t contain any tailwind (they are mangled) classes in the h1', () => {
-      const $h1 = cy.findByRole('heading', { level: 1 });
+      const $h1 = testComponentIframePage.getHeading();
 
       // check if the h1 don't have any of the classes in classArr
       classArr.forEach((className) => {
@@ -21,7 +24,7 @@ describe('IframePages', () => {
     });
 
     it('should contain a h1 with 5 classes', () => {
-      const $h1 = cy.findByRole('heading', { level: 1 })
+      const $h1 = testComponentIframePage.getHeading()
 
       // check if the h1 have exactly 5 classes
       $h1.invoke('attr', 'class').then((classes) => {
@@ -39,17 +42,17 @@ describe('IframePages', () => {
       cy.clearUsers().then(() => {
         cy.createUser(email, password);
         cy.login(email, password);
-        cy.visit('/iframe/src/components/windstatic/__test__/test.astro');
+        testComponentIframePage.visit();
       });
     });
 
     it('should contain an h1 including the text "Hello world"', () => {
-      const $h1 = cy.findByRole('heading', { level: 1 });
+      const $h1 = testComponentIframePage.getHeading();
       $h1.should('include.text', 'Hello world');
     });
 
     it('should contain all tailwind classes (not mangled) in the h1', () => {
-      const $h1 = cy.findByRole('heading', { level: 1 });
+      const $h1 = testComponentIframePage.getHeading();
 
       // check if the h1 don't have any of the classes in classArr
       classArr.forEach((className) => {
@@ -58,7 +61,7 @@ describe('IframePages', () => {
     });
 
     it('should contain a h1 with 5 classes', () => {
-      const $h1 = cy.findByRole('heading', { level: 1 })
+      const $h1 = testComponentIframePage.getHeading();
 
       // check if the h1 have exactly 5 classes
       $h1.invoke('attr', 'class').then((classes) => {
