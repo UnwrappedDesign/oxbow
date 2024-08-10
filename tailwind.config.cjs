9e3c9eb12db6082c98c58a4cf5b0452d61183b73
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-08-11 01:33:49 +0300

    added: Pricing x 8 new sections

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,20 +10,21 @@ module.exports = {
     },
     extend: {
       boxShadow: {
-        blackOnWhite: "0 10px 15px -3px rgba(0, 0, 0, 0.3), 0 4px 6px -4px rgba(0, 0, 0, 0.3) !important",
-        whiteOnWhite: "0 8px 12px -3px rgba(0, 0, 0, 0.05), 0 3px 5px -4px rgba(0, 0, 0, 0.05) !important"
+        blackOnWhite:
+          "0 10px 15px -3px rgba(0, 0, 0, 0.3), 0 4px 6px -4px rgba(0, 0, 0, 0.3) !important",
+        whiteOnWhite:
+          "0 8px 12px -3px rgba(0, 0, 0, 0.05), 0 3px 5px -4px rgba(0, 0, 0, 0.05) !important",
       },
       colors: {
         white: colors.white,
         gray: colors.gray,
         black: "#121212",
-
       },
     },
   },
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require('tailwind-scrollbar-hide')
+    require("tailwind-scrollbar-hide"),
   ],
 };
