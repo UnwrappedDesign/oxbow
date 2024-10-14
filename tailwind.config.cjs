2389698a6aed55ad98cb958d264a4c677c861af5
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-10-14 21:55:34 +0300

    refactored the Text component, removed heading component and Link component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,25 +4,32 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontWeight: {
-      light: "300",
-      normal: "400",
-      medium: "500",
-      semibold: "600",
-      bold: "700",
-      display: "350",
-    },
     extend: {
+
+
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        atipla: ["Atipla ND", ...defaultTheme.fontFamily.sans],
         mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
       },
       boxShadow: {
         blackOnWhite:
-          "0 10px 15px -3px rgba(0, 0, 0, 0.3), 0 4px 6px -4px rgba(0, 0, 0, 0.3) !important",
+          "0 10px 15px -3px rgba(0, 0, 0, 0.3), 0 4px 6px -4px rgba(0, 0, 0, 0.3)",
         whiteOnWhite:
-          "0 8px 12px -3px rgba(0, 0, 0, 0.05), 0 3px 5px -4px rgba(0, 0, 0, 0.05) !important",
+          "0 8px 12px -3px rgba(0, 0, 0, 0.05), 0 3px 5px -4px rgba(0, 0, 0, 0.05) ",
+
+
+        xs: "0 0 0 0.5px #0e3f7e0a, 0 0.5px 0.5px -0.25px #2a33450a, 0 1.5px 1.5px -0.75px #2a33460a, 0 3px 3px -1.5px #2a33460a, 0 6px 6px -3px #0e3f7e0a, 0 12px 12px -6px #0e3f7e0a;",
+        sm: "0 0 0 1px #0e3f7e0a, 0 1px 1px -0.5px #2a33450a, 0 3px 3px -1.5px #2a33460a, 0 6px 6px -3px #2a33460a, 0 12px 12px -6px #0e3f7e0a, 0 24px 24px -12px #0e3f7e0a;",
+        md: "0 0 0 2px #0e3f7e0a, 0 2px 2px -1px #2a33450a, 0 6px 6px -3px #2a33460a, 0 12px 12px -6px #2a33460a, 0 24px 24px -12px #0e3f7e0a, 0 48px 48px -24px #0e3f7e0a;",
+        lg: "0 0 0 4px #0e3f7e0a, 0 4px 4px -2px #2a33450a, 0 12px 12px -6px #2a33460a, 0 24px 24px -12px #2a33460a, 0 48px 48px -24px #0e3f7e0a, 0 96px 96px -48px #0e3f7e0a;",
+        xl: "0 0 0 6px #0e3f7e0a, 0 6px 6px -3px #2a33450a, 0 18px 18px -9px #2a33460a, 0 36px 36px -18px #2a33460a, 0 72px 72px -36px #0e3f7e0a, 0 144px 144px -72px #0e3f7e0a;",
+        "2xl":
+          "0 0 0 8px #0e3f7e0a, 0 8px 8px -4px #2a33450a, 0 24px 24px -12px #2a33460a, 0 48px 48px -24px #2a33460a, 0 96px 96px -48px #0e3f7e0a, 0 192px 192px -96px #0e3f7e0a;",
+        "3xl":
+          "0 0 0 10px #0e3f7e0a, 0 10px 10px -5px #2a33450a, 0 30px 30px -15px #2a33460a, 0 60px 60px -30px #2a33460a, 0 120px 120px -60px #0e3f7e0a, 0 240px 240px -120px #0e3f7e0a;",
+        // Add your default shadow
+        DEFAULT:
+          "0 0 0 1px #0e3f7e0a, 0 1px 1px -.5px #2a33450a, 0 3px 3px -1.5px #2a33460a, 0 6px 6px -3px #2a33460a, 0 12px 12px -6px #0e3f7e0a, 0 24px 24px -12px #0e3f7e0a;",
       },
       colors: {
         white: colors.white,
@@ -30,7 +37,7 @@ module.exports = {
         black: "#0f1a3a",
         accent: {
           25: "#f5fbff",
-         50: "#ebf7ff",
+          50: "#ebf7ff",
           100: "#d2edff",
           200: "#afdfff",
           300: "#78ceff",
@@ -50,17 +57,17 @@ module.exports = {
         },
         base: {
           25: "#fafafa",
-           '50': '#fafafa',
-        '100': '#f5f5f5',
-        '200': '#e5e5e5',
-        '300': '#d4d4d4',
-        '400': '#a3a3a3',
-        '500': '#737373',
-        '600': '#525252',
-        '700': '#404040',
-        '800': '#262626',
-        '900': '#171717',
-        '950': '#0a0a0a',
+          50: "#f9fafb",
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          400: "#9ca3af",
+          500: "#6b7280",
+          600: "#4b5563",
+          700: "#374151",
+          800: "#1f2937",
+          900: "#111827",
+          950: "#030712",
         },
         success: {
           25: "#edfcf5",
@@ -111,6 +118,6 @@ module.exports = {
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("tailwind-scrollbar-hide"),
-    require('tailwindcss-motion'),
+    require("tailwindcss-motion"),
   ],
 };
