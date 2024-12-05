e4f306e13e487f11a06b7fdf2ac5c071ae2d391c
Gabriel Perales <gabriel.perales@cabify.com>
2024-12-05 22:40:52 +0100

    fixed tailwind safelist

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,17 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: [
+    {
+      pattern: /^grid-cols-/,
+    },
+    {
+      pattern: /^grid-rows-/,
+    },
+    {
+      pattern: /^gap-/,
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {
