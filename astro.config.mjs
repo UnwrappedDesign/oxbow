70ebf688334615b6b1fcf992a39b2c43fbebeb90
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2025-01-03 11:35:07 +0200

    added email as a category

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import mdx from "@astrojs/mdx";
-import netlify from '@astrojs/netlify';
-import node from '@astrojs/node';
+import netlify from "@astrojs/netlify";
+import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-const { PUBLIC_APP_BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const { PUBLIC_APP_BASE_URL } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  "",
+);
 
-const localhost = PUBLIC_APP_BASE_URL?.includes('localhost') ?? false;
+const localhost = PUBLIC_APP_BASE_URL?.includes("localhost") ?? false;
 
 console.log("Loading app with PUBLIC_APP_BASE_URL: ", PUBLIC_APP_BASE_URL);
 console.log("Loading app with NODE_ENV: ", process.env.NODE_ENV);
@@ -18,24 +22,29 @@ console.log("Loading app with NODE_ENV: ", process.env.NODE_ENV);
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    '/free-tools/home': '/free-tools'
+    "/free-tools/home": "/free-tools",
   },
   markdown: {
     drafts: true,
     shikiConfig: {
-      theme: "css-variables"
-    }
+      theme: "css-variables",
+    },
   },
   build: {
-    inlineStylesheets: 'always',
+    inlineStylesheets: "always",
   },
   shikiConfig: {
     wrap: true,
     skipInline: false,
-    drafts: true
+    drafts: true,
   },
-  site: 'https://oxbowui.com',
-  integrations: [sitemap(), mdx(), alpinejs({ entrypoint: 'src/alpine' }), react()],
-  adapter: localhost ? node({mode: 'standalone'}) : netlify(),
-  output: 'server'
+  site: "https://oxbowui.com",
+  integrations: [
+    sitemap(),
+    mdx(),
+    alpinejs({ entrypoint: "src/alpine" }),
+    react(),
+  ],
+  adapter: localhost ? node({ mode: "standalone" }) : netlify(),
+  output: "server",
 });
