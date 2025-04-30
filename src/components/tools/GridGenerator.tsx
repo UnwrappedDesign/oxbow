3ae9837c91426e58ec93d9216496601f677eabd9
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2025-04-30 11:10:40 +0300

    removed dark mode

--- src/components/tools/GridGenerator.tsx
+++ src/components/tools/GridGenerator.tsx
@@ -136,7 +136,7 @@ export default function GridGenerator() {
     <div >
       <div className="w-full grid grid-cols-3 md:grid-cols-4 gap-2 ">
         <div className="flex flex-col gap-1">
-          <label htmlFor="columns" className="text-sm text-base-500 dark:text-base-300">
+          <label htmlFor="columns" className="text-sm text-base-500 ">
             Columns
           </label>
           <input
@@ -146,11 +146,11 @@ export default function GridGenerator() {
             max={12}
             value={columns}
             onChange={(e) => setColumns(Number(e.target.value))}
-            className="block w-full h-12 px-4 py-2 text-sm text-black bg-white border border-transparent appearance-none dark:text-white duration-300 dark:bg-white/5 backdrop-blur-2xl rounded-xl outline outline-base-100 dark:outline-white/5 placeholder-base-400 focus:border-base-100 dark:focus:border-base-900 focus:bg-transparent focus:outline-none focus:outline-accent-500 focus:outline-offset-2 focus:outline-2 sm:text-sm focus:ring-offset-base-950 dark:focus:ring-offset-base-950"
+            className="block w-full h-12 px-4 py-2 text-sm text-black bg-white border border-transparent appearance-none  duration-300 backdrop-blur-2xl rounded-xl outline outline-base-100 placeholder-base-400 focus:border-base-100 focus:bg-transparent focus:outline-none focus:outline-accent-500 focus:outline-offset-2 focus:outline-2 sm:text-sm focus:ring-offset-base-950"
           />
         </div>
         <div className="flex flex-col gap-1">
-          <label htmlFor="rows" className="text-sm text-base-500 dark:text-base-300">
+          <label htmlFor="rows" className="text-sm text-base-500 ">
             Rows
           </label>
           <input
@@ -160,11 +160,11 @@ export default function GridGenerator() {
             max={12}
             value={rows}
             onChange={(e) => setRows(Number(e.target.value))}
-            className="block w-full h-12 px-4 py-2 text-sm text-black bg-white border border-transparent appearance-none dark:text-white duration-300 dark:bg-white/5 backdrop-blur-2xl rounded-xl outline outline-base-100 dark:outline-white/5 placeholder-base-400 focus:border-base-100 dark:focus:border-base-900 focus:bg-transparent focus:outline-none focus:outline-accent-500 focus:outline-offset-2 focus:outline-2 sm:text-sm focus:ring-offset-base-950 dark:focus:ring-offset-base-950"
+            className="block w-full h-12 px-4 py-2 text-sm text-black bg-white border border-transparent appearance-none  duration-300 backdrop-blur-2xl rounded-xl outline outline-base-100 placeholder-base-400 focus:border-base-100 focus:bg-transparent focus:outline-none focus:outline-accent-500 focus:outline-offset-2 focus:outline-2 sm:text-sm focus:ring-offset-base-950"
           />
         </div>
         <div className="flex flex-col gap-1">
-          <label htmlFor="gap" className="text-sm text-base-500 dark:text-base-300">
+          <label htmlFor="gap" className="text-sm text-base-500 ">
             Gap
           </label>
           <input
@@ -174,7 +174,7 @@ export default function GridGenerator() {
             max={16}
             value={gap}
             onChange={(e) => setGap(Number(e.target.value))}
-            className="block w-full h-12 px-4 py-2 text-sm text-black bg-white border border-transparent appearance-none dark:text-white duration-300 dark:bg-white/5 backdrop-blur-2xl rounded-xl outline outline-base-100 dark:outline-white/5 placeholder-base-400 focus:border-base-100 dark:focus:border-base-900 focus:bg-transparent focus:outline-none focus:outline-accent-500 focus:outline-offset-2 focus:outline-2 sm:text-sm focus:ring-offset-base-950 dark:focus:ring-offset-base-950"
+            className="block w-full h-12 px-4 py-2 text-sm text-black bg-white border border-transparent appearance-none  duration-300 backdrop-blur-2xl rounded-xl outline outline-base-100 placeholder-base-400 focus:border-base-100 focus:bg-transparent focus:outline-none focus:outline-accent-500 focus:outline-offset-2 focus:outline-2 sm:text-sm focus:ring-offset-base-950"
           />
         </div>
       </div>
@@ -184,7 +184,7 @@ export default function GridGenerator() {
         <div
           ref={gridRef}
           className={`
-              grid font-mono text-black dark:text-white text-sm text-center font-bold  w-full h-full
+              grid font-mono text-black     text-sm text-center font-bold  w-full h-full
               grid-cols-${columns} grid-rows-${rows}
             `}
           style={{
@@ -198,7 +198,7 @@ export default function GridGenerator() {
             return (
               <div
                 key={index}
-                className="relative flex items-center justify-center p-8 text-2xl cursor-pointer bg-base-50  outline rounded-xl outline-base-100 dark:outline-white/5 dark:bg-white/5"
+                className="relative flex items-center justify-center p-8 text-2xl cursor-pointer bg-base-50  outline rounded-xl outline-base-100"
                 onClick={() => handleAddItem(x, y)}
               >
                 +
@@ -228,29 +228,29 @@ export default function GridGenerator() {
               onResizeStop(item.id, delta)
             }
             onDragStop={(e, data) => onDragStop(item.id, data)}
-            className="relative flex items-center justify-center p-4 cursor-pointer bg-base-100  outline rounded-xl outline-base-100 dark:outline-white/5 dark:bg-base-700"
+            className="relative flex items-center justify-center p-4 cursor-pointer bg-base-100  outline rounded-xl outline-base-100"
           >
             <button
               onClick={() => handleRemoveItem(item.id)}
               onTouchEnd={() => handleRemoveItem(item.id)}
-              className="absolute z-10 top-4 right-4  text-base-500 hover:text-black dark:text-hover:white"
+              className="absolute z-10 top-4 right-4  text-base-500 hover:text-black"
               aria-label={`Remove item ${item.id}`}
             >
               <X className="size-4" />
             </button>
-            <span className="text-black dark:text-white">{item.id.replace("item-", "")}</span>
+            <span className="text-black ">{item.id.replace("item-", "")}</span>
             <div className="absolute flex items-center justify-center rounded-bl bottom-4 right-4 ">
-              <ArrowDownRight className="text-black size-4 dark:text-white" />
+              <ArrowDownRight className="text-black size-4 " />
             </div>
           </Rnd>
         ))}
       </div>
       <div className="flex flex-col justify-between w-full pt-4 md:flex-row md:items-center">
-        <h3 className="font-mono text-black uppercase dark:text-white ">Get your code</h3>
+        <h3 className="font-mono text-black uppercase ">Get your code</h3>
         <div className="flex items-center gap-2 ">
           <button
             className={`
-                    flex items-center text-center justify-center font-semibold duration-500 ease-in-out transition focus:outline-2 text-black bg-base-50 hover:bg-base-100 focus:outline-base-200 dark:text-white dark:bg-base-800 dark:hover:bg-base-900 dark:focus:outline-base-900 h-7.5 px-6 py-2 text-xs rounded-md pointer-events-auto outline-base-100 !dark:outline-base-700
+                    flex items-center text-center justify-center font-semibold duration-500 ease-in-out transition focus:outline-2 text-black bg-base-50 hover:bg-base-100 focus:outline-base-200        h-7.5 px-6 py-2 text-xs rounded-md pointer-events-auto outline-base-100  
                     ${format === "html" ? "!outline-base-700" : " "}
                   `}
             onClick={() => setFormat("html")}
@@ -259,7 +259,7 @@ export default function GridGenerator() {
           </button>
           <button
             className={`
-                  flex items-center text-center justify-center font-semibold duration-500 ease-in-out transition focus:outline-2 text-black bg-base-50 hover:bg-base-100 focus:outline-base-200 dark:text-white dark:bg-base-800 dark:hover:bg-base-900 dark:focus:outline-base-900 h-7.5 px-6 py-2 text-xs rounded-md pointer-events-auto outline-base-100 !dark:outline-base-700
+                  flex items-center text-center justify-center font-semibold duration-500 ease-in-out transition focus:outline-2 text-black bg-base-50 hover:bg-base-100 focus:outline-base-200        h-7.5 px-6 py-2 text-xs rounded-md pointer-events-auto outline-base-100  
                     ${format === "jsx" ? "!outline-base-700" : ""}
                   `}
             onClick={() => setFormat("jsx")}
@@ -267,21 +267,21 @@ export default function GridGenerator() {
             JSX
           </button>
           <button
-            className="flex items-center text-center justify-center font-semibold duration-500 ease-in-out transition focus:outline-2 text-black bg-base-50 hover:bg-base-100 focus:outline-base-200 dark:text-white dark:bg-base-800 dark:hover:bg-base-900 dark:focus:outline-base-900 h-7.5 px-6 py-2 text-xs rounded-md pointer-events-auto outline-base-100 !dark:outline-base-700"
+            className="flex items-center text-center justify-center font-semibold duration-500 ease-in-out transition focus:outline-2 text-black bg-base-50 hover:bg-base-100 focus:outline-base-200        h-7.5 px-6 py-2 text-xs rounded-md pointer-events-auto outline-base-100  "
             onClick={handleReset}
           >
             Reset
           </button>
           <button
-            className="fflex items-center text-center justify-center  font-semibold duration-500 ease-in-out transition focus:outline-2 text-black bg-base-50 hover:bg-base-100 focus:outline-base-200 dark:text-white dark:bg-base-800 dark:hover:bg-base-900 dark:focus:outline-base-900 h-7.5 px-6 py-2 text-xs rounded-md pointer-events-auto outline-base-100 !dark:outline-base-700   w-24"
+            className="fflex items-center text-center justify-center  font-semibold duration-500 ease-in-out transition focus:outline-2 text-black bg-base-50 hover:bg-base-100 focus:outline-base-200        h-7.5 px-6 py-2 text-xs rounded-md pointer-events-auto outline-base-100     w-24"
             onClick={handleCopy}
           >
             {isCopied ? "Copied!" : "Copy"}
           </button>
         </div>
       </div>
-      <div className="p-4 mt-2 bg-white  dark:bg-base-900 outline rounded-xl outline-base-100 dark:outline-white/5">
-        <pre className="overflow-x-auto text-xs text-base-500 dark:text-base-400 ">
+      <div className="p-4 mt-2 bg-white  outline rounded-xl outline-base-100">
+        <pre className="overflow-x-auto text-xs text-base-500 ">
           <code>{generateCode}</code>
         </pre>
       </div>
