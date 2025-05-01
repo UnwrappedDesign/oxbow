ce2bce2d8655bd80c756c962e96af6ad0b23190f
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2025-05-02 01:48:08 +0300

    moved to polar

--- src/components/tools/GradientGenerator.tsx
+++ src/components/tools/GradientGenerator.tsx
@@ -77,13 +77,13 @@ export default function GradientGenerator() {
   };
   return (
     <div id="generator">
-      <div className="z-10 bg-white lg:sticky lg:top-2 ">
-        <div className="flex items-center bg-white ">
+      <div className="z-10 bg-background lg:sticky lg:top-2 ">
+        <div className="flex items-center bg-background ">
           <label className="sr-only">Mode</label>
           <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-2 ">
             <button
               className={`
-                            flex items-center text-center justify-center   duration-500 ease-in-out transition-colors focus:outline-2 text-black bg-base-100 hover:bg-base-200 focus:outline-accent-500          h-10 px-6 py-3 text-base  rounded-lg w-full
+                            flex items-center text-center justify-center   duration-500 ease-in-out transition-colors focus:outline-2 text-black bg-white hover:bg-base-100 focus:outline-accent-500          h-10 px-6 py-3 text-base  rounded-lg w-full
                               ${mode === "background" ? "!outline-base-700" : "text-base-500 "}
                             `}
               onClick={() => setMode("background")}
@@ -92,7 +92,7 @@ export default function GradientGenerator() {
             </button>
             <button
               className={`
-                          flex items-center text-center justify-center   duration-500 ease-in-out transition-colors focus:outline-2 text-black bg-base-100 hover:bg-base-200 focus:outline-accent-500          h-10 px-6 py-3 text-base  rounded-lg w-full
+                          flex items-center text-center justify-center   duration-500 ease-in-out transition-colors focus:outline-2 text-black bg-white hover:bg-base-100 focus:outline-accent-500          h-10 px-6 py-3 text-base  rounded-lg w-full
                               ${mode === "text" ? "!outline-base-700" : "text-base-500"}
                             `}
               onClick={() => setMode("text")}
@@ -100,14 +100,14 @@ export default function GradientGenerator() {
               Text
             </button>
             <button
-              className="flex items-center justify-center w-full h-10 px-6 py-3 text-base text-center text-black rounded-lg  duration-500 ease-in-out transition-colors focus:outline-2 bg-base-100 hover:bg-base-200 focus:outline-accent-500"
+              className="flex items-center justify-center w-full h-10 px-6 py-3 text-base text-center text-black rounded-lg  duration-500 ease-in-out transition-colors focus:outline-2 bg-white hover:bg-base-100 focus:outline-accent-500"
               onClick={copyToClipboard}
             >
               <span>{copied ? "Copied!" : "Copy"}</span>
             </button>
           </div>
         </div>
-        <div className="w-full pt-2 bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 ">
+        <div className="w-full pt-2 bg-background grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 ">
           {mode === "text" && (
             <div className="w-full">
               <label className="sr-only">Your Text</label>
@@ -142,31 +142,31 @@ export default function GradientGenerator() {
             value={generateTwGradient()}
           />
         </div>
-        <div className="pt-2 bg-white ">
+        <div className="pt-2 bg-background ">
           <div
-            className={`flex items-center justify-center relative w-full  lg:sticky  z-90  p-24 outline rounded-xl outline-base-100        overflow-hidden    ${generateTwGradient()}`}
+            className={`flex items-center justify-center relative w-full  lg:sticky  z-90  p-24  rounded-lg        overflow-hidden    ${generateTwGradient()}`}
           >
             {mode === "text" ? (
               <p className="text-5xl font-bold text-center">{text}</p>
             ) : null}
           </div>
         </div>
-        <div className="flex items-center py-4 mt-2 bg-white  gap-x-2 outline rounded-xl outline-base-100">
+        <div className="flex items-center py-2  bg-background  gap-x-2 ">
           <button
-            className={` flex items-center text-center justify-center  duration-500 ease-in-out transition-colors focus:outline-2 text-black bg-base-100 hover:bg-base-200 focus:outline-accent-500          h-10 px-6 py-3 text-base  rounded-lg w-full ${tab === "from" ? "!outline-base-500 " : ""}`}
+            className={` flex items-center text-center justify-center  duration-500 ease-in-out transition-colors focus:outline-2 text-black bg-white hover:bg-base-50 focus:outline-accent-500          h-10 px-6 py-3 text-base  rounded-lg w-full ${tab === "from" ? "!outline-base-500 " : ""}`}
             onClick={() => setTab("from")}
           >
             From Color
           </button>
           <button
-            className={`  flex items-center text-center justify-center  duration-500 ease-in-out transition-colors focus:outline-2 text-black bg-base-100 hover:bg-base-200 focus:outline-accent-500          h-10 px-6 py-3 text-base  rounded-lg w-full ${tab === "to" ? "!outline-base-700" : ""}`}
+            className={`  flex items-center text-center justify-center  duration-500 ease-in-out transition-colors focus:outline-2 text-black bg-white hover:bg-base-50 focus:outline-accent-500          h-10 px-6 py-3 text-base  rounded-lg w-full ${tab === "to" ? "!outline-base-700" : ""}`}
             onClick={() => setTab("to")}
           >
             To Color
           </button>
         </div>
       </div>
-      <div className="py-8 mt-4 bg-white  grid grid-cols-4 gap-x-4 md:grid-cols-8 gap-y-12 lg:grid-cols-12 outline rounded-xl outline-base-100">
+      <div className="p-8 mt-4 bg-white  grid grid-cols-4 gap-x-4 md:grid-cols-8 gap-y-12 lg:grid-cols-12 outline rounded-xl outline-base-100">
         {tailwindColors.map((color) => (
           <div>
             <p className="font-mono text-xs uppercase text-base-500 ">
